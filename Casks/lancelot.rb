9801291052ba7877668b89980447d5bce5b7f342
boredcoder411 <taamarcos@gmail.com>
2025-01-04 20:36:18 +0100

    update to 1.1

--- Casks/lancelot.rb
+++ Casks/lancelot.rb
@@ -1,6 +1,6 @@
 cask "lancelot" do
-  version "1.0"
-  sha256 "f79b791a03f427d086c3e1dd7a5aa36b7e408f129d5effb1485045a0c7c2d4cf"
+  version "v1.1"
+  sha256 "c3443c88863b555d18368a68bfb4bbf78a7692661549cdcb58e8a01fb218167f"
 
   url "https://github.com/Orbitalfetch/lancelot/releases/download/#{version}/Lancelot.zip"
   name "Lancelot"
