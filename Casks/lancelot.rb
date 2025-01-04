2b916f62451b8c7a780cbbade71848b55e757965
boredcoder411 <105937655+boredcoder411@users.noreply.github.com>
2025-01-04 15:28:25 +0100

    Create lancelot.rb

---
+++ Casks/lancelot.rb
@@ -0,0 +1,10 @@
+cask "lancelot" do
+  version "1.0"
+  sha256 "f79b791a03f427d086c3e1dd7a5aa36b7e408f129d5effb1485045a0c7c2d4cf"
+
+  url "https://github.com/Orbitalfetch/lancelot/releases/download/#{version}/Lancelot.zip"
+  name "Lancelot"
+  homepage "https://lancelot.orbitalfetch.tech"
+
+  app "Lancelot.app"
+end
